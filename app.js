02f5fde02664b60999546bfe275d8c8cdcefc729
Derek Bohman <derekbohman@Dereks-MacBook-Pro.local>
2022-05-17 18:55:25 -0700

    Still trying to solve problem six.

--- app.js
+++ app.js
@@ -207,6 +207,11 @@ console.log("Problem Five Answer: ", problemFiveResults);
 
 //6a. Create a function that will return an array of only the names of the cuisine types. Ie ['Italian', 'Mexican', ...]
 //Map
+
+let problemSixResults = dishes[2].map(function(cuisine)) {
+    return cuisine;
+}
+
 // BONUS: (come back to this after finishing all)
 //6b. Use the filter method to eliminate duplicates, leaving only distinct values in the array
 
